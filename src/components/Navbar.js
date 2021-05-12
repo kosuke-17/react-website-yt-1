6a13366a6b63cf9808c6799113fe7c5d5dffb523
kosuke-17 <tamutamu17@icloud.com>
2021-05-12 22:06:22 +0900

    トップページの作成

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ function Navbar() {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
   return (
     <>
@@ -14,8 +15,30 @@ function Navbar() {
           TRYL <i className='fab fa-typo3' />
           </Link>  
           <div className="menu-icon" onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+            <i className={click ? 'fas fa-balance-scale-right' : 'fas fa-balance-scale'} />
           </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/services' className="nav-links" onClick={closeMobileMenu}>
+                Services
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/products' className="nav-links" onClick={closeMobileMenu}>
+                Products
+              </Link>
+            <li className='nav-item'>
+              <Link to='/sign-up' className="nav-links-mobile" onClick={closeMobileMenu}>
+                Sign Up
+              </Link>
+            </li>
+            </li>
+          </ul>
         </div>
       </nav>
     </>

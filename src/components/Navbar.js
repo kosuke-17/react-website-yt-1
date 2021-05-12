bbce93c7e619c477f2d137b41b67efdac3c0ce47
kosuke-17 <tamutamu17@icloud.com>
2021-05-12 21:57:15 +0900

    iconクリック

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,11 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Link} from 'react-router-dom';
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
   return (
     <>
       <nav className="navbar">
@@ -9,8 +13,8 @@ function Navbar() {
         <Link to="/" className="navbar-logo">
           TRYL <i className='fab fa-typo3' />
           </Link>  
-          <div className="menu-icon">
-            
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
         </div>
       </nav>

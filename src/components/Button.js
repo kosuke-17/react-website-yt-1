a86318ee9eb82ce1c3fbe8764a9e1d30a70e4a1b
kosuke-17 <tamutamu17@icloud.com>
2021-05-13 11:41:16 +0900

    ヘッダー作成

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
 
-const STYLES = ['btn-primary', 'btn--outline'];
+const STYLES = ['btn--primary', 'btn--outline'];
 
-const SIZES = ['btn-medium', 'btn--large'];
+const SIZES = ['btn--medium', 'btn--large'];
 
 export const Button = ({
   children,
   type,
   onClick,
   buttonStyle,
-  buttonSite
+  buttonSize
 }) => {
 const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
 
@@ -23,5 +23,5 @@ return (
       {children}
     </button>
   </Link>
-)
+);
 };
